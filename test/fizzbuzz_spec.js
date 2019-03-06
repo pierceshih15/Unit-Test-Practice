35de546a67f5a4f51f19a17a8e7348bee21e55b2
pierce <pierceshih@gmail.com>
2019-03-06 22:39:39 +0800

    wording adjustment

--- test/fizzbuzz_spec.js
+++ test/fizzbuzz_spec.js
@@ -1,23 +1,23 @@
 var should = chai.should()
 
-describe('Test fizzBuzz function', function () {
+describe('Unit-Test for FizzBuzz function', function () {
   // 測試案例一：若該整數能被 3 整除，回傳 Fizz；
-  it('should return Fizz if number can be evenly divisible by 3', function () {
+  it('should return Fizz when number can be evenly divisible by 3', function () {
     let result = fizzBuzz(9)
     result.should.be.equal('Fizz')
   })
   // 測試案例二：若該整數能被 5 整除，回傳 Buzz；
-  it('should return Buzz if number can be evenly divisible by 5', function () {
+  it('should return Buzz when number can be evenly divisible by 5', function () {
     let result = fizzBuzz(10)
     result.should.be.equal('Buzz')
   })
   // 測試案例三：若該整數能被 3 和 5 同時整除，回傳 FizzBuzz；
-  it('should return FizzBuzz if number can be evenly divisible by 3 and 5 ', function () {
+  it('should return FizzBuzz when number can be evenly divisible by 3 and 5 ', function () {
     let result = fizzBuzz(15)
     result.should.be.equal('FizzBuzz')
   })
   // 測試案例四：若都不能整除，回傳該整數。
-  it("should return Buzz if number can't be evenly divisible by 3 and 5", function () {
+  it("should return Buzz when number can't be evenly divisible by 3 and 5", function () {
     let result = fizzBuzz(7)
     result.should.be.equal(7)
   })
